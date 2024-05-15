e2f4904c77b44d3930d827a08328b3062af26e26
Wasawsky <wasawskycore@gmail.com>
2024-05-14 19:13:06 -0500

    First Commit

---
+++ src/main/java/com/ml/info/geoipservice/GeoipserviceApplication.java
@@ -0,0 +1,13 @@
+package com.ml.info.geoipservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoipserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeoipserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ml/info/geoipservice/GeoipserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ml.info.geoipservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoipserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
